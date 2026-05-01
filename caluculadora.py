6683c9f777ec60a47a8c7005d8231be129876325
kaiopaula1113-create <kaiopaula1113@gmail.com>
2026-05-01 10:53:52 -0300

    Add files via upload

---
+++ caluculadora.py
@@ -0,0 +1,13 @@
+value = float(input('digite um número:'))
+value2 = float(input('digite outro:'))
+sinal = input('escolha um sinal:')
+if sinal == '+':
+  print(value + value2)
+if sinal == '-':
+    print(value - value2)
+if sinal == '*':
+    print(value * value2)
+if sinal == '/':
+    print(value / value2)
+if sinal == '%':
+    print(value % value2)
